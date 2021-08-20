3e314f45b12b56a50f8cdae1946aedabd1d8821e
daniele-frade <daniele.fradee@gmail.com>
2021-08-20 18:21:49 -0400

    aula 01 e 02

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Title = () => {
+  return <h1>Meu Titulo</h1>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title />
 }
